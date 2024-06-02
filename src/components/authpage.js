9b8c520c5b2e39b94c23138789f4c5dc2223a21b
Артур Сагадеев <arturik_sagadeev@mail.ru>
2024-06-03 01:49:17 +0500

    ds

--- src/components/authpage.js
+++ src/components/authpage.js
@@ -1,42 +1,112 @@
 import React, { useState } from 'react';
+import axios from "axios";
+import { useNavigate, Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const AuthPage = () => {
+    const navigate = useNavigate();
     const [isSignUp, setIsSignUp] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailExists, setEmailExists] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSignup = (e) => {
         e.preventDefault();
-        // ... ваш обработчик регистрации
+
+        if (isEmailRegistered(email)) {
+            setEmailExists(true);
+            return;
+        }
+
+        axios.post('https://?', { email, password })
+            .then(response => {
+                alert('Регистрация прошла успешно!');
+                setIsSignUp(false);
+            })
+            .catch(error => {
+                alert('Что-то пошло не так при регистрации.');
+                console.error(error);
+            });
     };
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
-        // ... ваш обработчик аутентификации
+
+        // Отправка данных на сервер для аутентификации пользователя
+        try {
+            const response = await axios.post('https://?', { email, password });
+            alert('Вход выполнен!');
+
+            // Сохранение токена доступа в localStorage или Cookies
+            localStorage.setItem('token', response.data.token);
+
+            navigate('/');
+        } catch (error) {
+            alert('Неправильный email или пароль.');
+            console.error(error);
+        }
+    };
+
+    const isEmailRegistered = async (email) => {
+        try {
+            const response = await axios.post('https://?', { email });
+            return response.data.exists;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
     };
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-200">
+        <div className="flex justify-center items-center h-screen bg-gray-200 relative">
+            <Link to="/" className="fixed top-2 left-2 text-blue-700 px-4 py-2 rounded border border-blue-700">Назад</Link>
+
             <div className="bg-blue-500 p-6 rounded-lg shadow-lg text-white">
                 <div className="text-center">
-                    {isSignUp ? (
-                        <form onSubmit={handleSignup} className="space-y-4">
-                            <h2>Регистрация</h2>
-                            <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 rounded border" />
-                            <label htmlFor="password">Пароль:</label>
-                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 rounded border" />
-                            <button type="submit" className="w-full bg-blue-700 text-white rounded py-2">Зарегистрироваться</button>
-                        </form>
-                    ) : (
+                    {!isSignUp ? (
                         <form onSubmit={handleLogin} className="space-y-4">
                             <h2>Авторизация</h2>
-                            <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 rounded border" />
-                            <label htmlFor="password">Пароль:</label>
-                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 rounded border" />
+                            <label htmlFor="email" style={{ color: "black" }}>Email:</label>
+                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 rounded border" style={{ color: "black" }} />
+                            <label htmlFor="password" style={{ color: "black" }}>Пароль:</label>
+                            <div className="relative">
+                                <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 rounded border" style={{ color: "black" }} />
+                                <span
+                                    onClick={toggleShowPassword}
+                                    className="absolute top-2 right-3 cursor-pointer"
+                                >
+                                    {showPassword ? <FaEyeSlash color="black" /> : <FaEye color="black" />}
+                                </span>
+                            </div>
                             <button type="submit" className="w-full bg-blue-700 text-white rounded py-2">Войти</button>
                         </form>
+                    ) : (
+                        <form onSubmit={handleSignup} className="space-y-4">
+                            <h2>Регистрация</h2>
+                            <label htmlFor="email" style={{ color: "black" }}>Email:</label>
+                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 rounded border" style={{ color: "black" }} />
+                            <label htmlFor="password" style={{ color: "black" }}>Пароль:</label>
+                            <div className="relative">
+                                <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 rounded border" style={{ color: "black" }} />
+                                <span
+                                    onClick={toggleShowPassword}
+                                    className="absolute top-2 right-3 cursor-pointer"
+                                >
+                                    {showPassword ? <FaEyeSlash color="black" /> : <FaEye color="black" />}
+                                </span>
+                            </div>
+                            <button type="submit" className="w-full bg-blue-700 text-white rounded py-2">Зарегистрироваться</button>
+                        </form>
+                    )}
+                    {emailExists && (
+                        <div className="absolute bottom-2 right-2 bg-red-500 text-white p-2 rounded">
+                            Данный email уже используется!
+                        </div>
                     )}
                     <button onClick={() => setIsSignUp(!isSignUp)} className="text-center mt-4 py-2 w-full rounded bg-blue-700 text-white">
                         {isSignUp ? 'Вход' : 'Регистрация'}
